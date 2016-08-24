2fd9954d2e3dbff07b63cdc93f8c8ef05ebcdb76
zhangsiy <jeffzhang_misc@hotmail.com>
2016-08-24 00:05:10 -0400

    BM-1230 first attempt to allow publishing to exponent with environments

---
+++ deploy_exponent.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo "Start npm install..."
+sudo npm install
+if [ $? -ne 0 ]; then
+    echo "Failed perform npm install"
+    exit 1
+fi
+
+echo "Switching to the target environment: $1"
+./switch_env.sh $1
+if [ $? -ne 0 ]; then
+    echo "Failed to switch environment to '$1'"
+    exit 1
+fi
+
+echo "Publishing to Exponent Host.."
+deployed=1
+
+# Below uses the Exponent XDE's CLI, see App's wiki page
+# for installation instructions
+
+# Login to Exponent
+exp login -u zhangsiy -p 1234567890
+
+# Start EXP server
+exp start
+
+# Publish!
+exp publish
+
+# Stop the server
+exp stop
+
+# Log out
+exp logout
+
+if [ $? -ne 0 ]; then
+    echo "Failed to push to Exponent Host"
+    deployed=0
+fi
+
+echo "Switching back to environment: dev"
+./switch_env.sh dev
+if [ $? -ne 0 ]; then
+    echo "Failed to switch environment to 'dev'"
+    exit 1
+fi
+
+if [ $deployed -ne 1 ]; then
+    echo "FAILURE: Deployment failed!"
+    exit 1
+fi
+
+echo "SUCCESS: The deployment has been completed!"
--- switch_env.sh
+++ switch_env.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-echo "Start npm install..."
-sudo npm install
+if [ ! -d "./env_settings/$1" ]; then
+  echo "Failed to locate configurations for the specified envrionment name '$1'!"
+  exit 1
+fi
 
-echo "Package up artifacts..."
-zip ./buildartifact.zip -r * .[^.]*
+echo "START: Switching to environment: $1"
 
-echo "Running eb deploy..."
-eb deploy
+echo ".. Backing up current configurations..."
+rm -rf ./env_switch_backup
+mkdir ./env_switch_backup
+mv ./exp.json ./env_switch_backup/exp.json
+mv ./app_settings.json ./env_switch_backup/app_settings.json
 
-echo "Cleanup packages..."
-rm ./buildartifact.zip
+echo ".. Deploying configurations from target environment: $1"
+cp ./env_settings/$1/exp.json ./exp.json
+cp ./env_settings/$1/app_settings.json ./app_settings.json
 
-echo "The deployment has been completed!"
+echo ".. Cleaning up the backup configurations"
+rm -rf ./env_switch_backup
+
+echo "COMPLETE: Switched to environment: $1"
